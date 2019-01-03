47fb67c12dd2e66c8f41123c0142bc0b2df51c27
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:33:08 +0700

    Add files via upload

---
+++ Program konversi dolar ke rupiah.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float dolar;
+    cout<<"konversi uang dolar ke rupiah"<<endl;
+    cout<<"masukan jumlah uang dolar $ = ";
+    cin>>dolar;
+    //rumus menghitung konversi dolar ke rupiah
+    cout<<"Nilai rupiah dari sejumlah dolar di atas adalah = "<<dolar * 13000;
+    return 0;
+}
